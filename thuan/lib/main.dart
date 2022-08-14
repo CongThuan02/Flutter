47b96bc182428d46bd1031f89c096fcd18dbd767
thuanmystic <2021050033@student.humg.edu.vn>
2022-08-14 10:34:50 +0700

    thuan

---
+++ thuan/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'fultter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('toan, van anh')),
+        body: Container(
+            color: Colors.yellowAccent,
+            child: Column(children: [
+              Expanded(
+                  child: Container(
+                      color: Colors.yellowAccent,
+                      child: Text('1',
+                          style: TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.red))))
+            ])));
+  }
+}
