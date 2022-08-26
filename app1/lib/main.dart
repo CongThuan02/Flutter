25fb8bbd527961d65ceb53c92630d66718651c66
thuanmystic <2021050033@student.humg.edu.vn>
2022-08-26 10:09:45 +0700

    2606

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -1,18 +1,62 @@
-
 import 'package:flutter/material.dart';
-import 'package:movie_db_app/src/ui/home_screen.dart';
 
 void main() {
-  runApp(SportifyApp());
+  runApp(MyApp());
 }
 
-class SportifyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    debugShowCheckedModeBanner: false;
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      title: 'Title of Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.directions_car),
+              ),
+              Tab(
+                icon: Icon(Icons.directions_transit),
+              ),
+              Tab(
+                icon: Icon(Icons.directions_bike),
+              ),
+            ],
+          ),
+          title: Text('Flutter Tabs Example'),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text("Car"),
+            ),
+            Center(
+              child: Text("Transit"),
+            ),
+            Center(
+              child: Text("Bike"),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
